43b30515b2a78a78396e39347fb79a25ec6f9cf0
rajat-28 <106760085+rajat-28@users.noreply.github.com>
2026-03-24 20:52:46 +0530

    fixed ultrasound form

--- src/modules/ultrasound/Ultrasound.jsx
+++ src/modules/ultrasound/Ultrasound.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import UltrasoundList from './UltrasoundList'
 import UltrasoundPatientView from './UltrasoundPatientView'
+import UltrasoundForm from './UltrasoundForm'
 
 export default function Ultrasound() {
   return (
     <Routes>
       <Route index              element={<UltrasoundList />} />
+      <Route path="new"         element={<UltrasoundForm />} />
+      <Route path="edit/:id"    element={<UltrasoundForm />} />
       <Route path="patient/:id" element={<UltrasoundPatientView />} />
     </Routes>
   )
